8e5e12db8a7d70cbeab2c872351a262c34ba7ec0
Dương Kim Sang <SangDK.B24CE236@stu.ptit.edu.vn>
2026-01-28 12:57:09 +0000

    "edit toml"

--- Pipeline/ingest_data.py
+++ Pipeline/ingest_data.py
@@ -4,6 +4,7 @@
 import pandas as pd
 from tqdm.auto import tqdm
 from sqlalchemy import create_engine
+import click
 
 dtype = {
     "VendorID": "Int64",
@@ -29,16 +30,18 @@ parse_dates = [
     "tpep_dropoff_datetime"
 ]
 
-def run():
-    year=2021
-    month=1
-
-    pg_user='root'
-    pg_pw='root'
-    pg_host='localhost'
-    pg_port=5432
-    pg_db='ny_taxi'
-
+@click.command()
+@click.option('--year', type=int, default=2021, help='Year of the data')
+@click.option('--month', type=int, default=1, help='Month of the data')
+@click.option('--pg-user', default='root', help='PostgreSQL username')
+@click.option('--pg-pw', default='root', help='PostgreSQL password')
+@click.option('--pg-host', default='localhost', help='PostgreSQL host')
+@click.option('--pg-port', type=int, default=5432, help='PostgreSQL port')
+@click.option('--pg-db', default='ny_taxi', help='PostgreSQL database name')
+@click.option('--target-table', default='yellow_taxi_data', help='Target table name')
+@click.option('--chunk-size', type=int, default=100000, help='Chunk size for reading data')
+def run(year, month, pg_user, pg_pw, pg_host, pg_port, pg_db, target_table, chunk_size):
+    """Ingest NYC taxi data into PostgreSQL database"""
     prefix="https://github.com/DataTalksClub/nyc-tlc-data/releases/download/yellow"
     url=f"{prefix}/yellow_tripdata_{year:04d}-{month:02d}.csv.gz"
 
@@ -49,15 +52,12 @@ def run():
         dtype=dtype,
         parse_dates=parse_dates,
         iterator=True,
-        chunksize=100000
+        chunksize=chunk_size
     )
     for i, df_chunk in enumerate(tqdm(df_iter)):
         if_exists = 'replace' if i == 0 else 'append'
-        df_chunk.to_sql(name='yellow_taxi_data',con=engine,if_exists=if_exists)
+        df_chunk.to_sql(name=target_table,con=engine,if_exists=if_exists)
+        print(f"Inserted chunk {i+1} into the database.") 
 
-        
 if __name__ == "__main__":
     run()
-
-
-
